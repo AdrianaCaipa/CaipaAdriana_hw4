e0241c167c89057612ea8370d51af46f79cb4e7e
AdrianaCaipa <42391099+AdrianaCaipa@users.noreply.github.com>
2018-11-17 16:30:05 -0500

    RungeKutta de angulo 45

---
+++ ODE.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+int glob;
+#define PI 3.14159
+#define c 0.2
+#define g 10
+#define m 0.2
+
+double dxdt(double t, double x, double x_dev);
+double dydt(double t, double y, double y_dev);
+double dxdt2(double t, double x, double angulo,double x_dev);
+double dydt2(double t, double y, double angulo, double y_dev);
+void rungeKutta_4(double *x, double *y, double *z, double h);
+
+
+double dxdt(double t, double x, double x_dev){
+	return x_dev;
+}
+
+double dydt(double t, double y, double y_dev){
+	return y_dev;
+}
+
+//Componente en x
+double dxdt2(double t, double x, double angulo,double x_dev){
+	return 0-(c/m)*(c/m)*(x_dev*cos(angulo*PI/180));
+}
+//componente en y
+double dydt2(double t, double y, double angulo, double y_dev){
+	return -1.0/1.2*g-(c/m)*(y_dev*sin(angulo*PI/180));
+}	
+
+void rungeKutta_4(double *x, double *y, double *x_dev,double *y_dev, double h){
+  double x_inicial, y_inicial, ydev_inicial;
+  double xdev_inicial;
+  
+  double kx_1, kx_2, kx_3, kx_4;
+  double ky_1, ky_2, ky_3, ky_4;
+  double kdx_1, kdx_2,kdx_3, kdx_4;
+  double kdy_1, kdy_2,kdy_3, kdy_4;
+  
+	
+  x_inicial = *x;
+  y_inicial = *y;
+  xdev_inicial = *x_dev;
+  ydev_inicial = *y_dev;
+  double t=0;
+  double angulo= 45;
+  kx_1  = dxdt(t,x_inicial,xdev_inicial);
+  ky_1  = dydt(t,y_inicial,ydev_inicial);
+  kdx_1 = dxdt2(t,x_inicial,angulo,xdev_inicial);
+  kdy_1 = dydt2(t,y_inicial,angulo,ydev_inicial);
+  
+  kx_2  = dxdt(t+h*0.5, x_inicial+kx_1*0.5, xdev_inicial+kdx_1*0.5);
+  ky_2  = dydt(t+h*0.5, y_inicial+ky_1*0.5, ydev_inicial+kdy_1*0.5);
+  kdx_2 = dxdt2(t+h*0.5, x_inicial+kx_1*0.5,angulo,xdev_inicial+kdx_1*0.5);
+  kdy_2 = dydt2(t+h*0.5, y_inicial+ky_1*0.5,angulo,ydev_inicial+kdy_1*0.5);
+		
+  kx_3  = dxdt(t+h*0.5, x_inicial+kx_2*0.5, xdev_inicial+kdx_2*0.5);
+  ky_3  = dydt(t+h*0.5, y_inicial+ky_2*0.5, ydev_inicial+kdy_2*0.5);
+  kdx_3 = dxdt2(t+h*0.5, x_inicial+kx_2*0.5,angulo,xdev_inicial+kdx_2*0.5);
+  kdy_3 = dydt2(t+h*0.5, y_inicial+ky_2*0.5,angulo,ydev_inicial+kdy_2*0.5);
+
+  kx_4  = dxdt(t+h, x_inicial+kx_3,xdev_inicial+kdx_3);
+  ky_4  = dydt(t+h, y_inicial+ky_3,ydev_inicial+kdy_3);
+  kdx_4 = dxdt2(t+h, x_inicial+kx_3, angulo,xdev_inicial+kdy_3);
+  kdy_4 = dydt2(t+h, y_inicial+ky_3,angulo, ydev_inicial+kdy_3);
+
+  x_inicial = x_inicial + h;
+  y_inicial = y_inicial + h * (ky_1 + 2*ky_2 + 2*ky_3 + ky_4)/6.0;
+  xdev_inicial = xdev_inicial + h * (kdx_1+2*kdx_2+2*kdx_3+kdx_4)/6.0;
+  ydev_inicial = ydev_inicial + h * (kdy_1+2*kdy_2+2*kdy_3+kdy_4)/6.0;
+
+   t =  t + h ;
+  *x = x_inicial;
+  *y = y_inicial;
+  *x_dev = xdev_inicial;
+  *y_dev= ydev_inicial;
+  
+}
+
+
+int main(){
+  double x=0;
+  double y=0;
+  double t=0;
+  double h=0.1;
+  double angulo=45;
+  double x_dev= 300*cos(angulo*PI/180);
+  double y_dev= 300*sin(angulo*PI/180);
+  ofstream outfile("datos45.txt");
+  while(y>=0){
+    outfile << t <<" " << x << " " << y << " " << x_dev << " " << y_dev <<endl;		
+	cout <<    t <<" " << x << " " << y << " " << x_dev << " " << y_dev <<endl;
+    rungeKutta_4(&x, &y, &x_dev, &y_dev, h);
+  }
+  outfile.close();
+  
+  int angulos[7] = { 10, 20, 30, 40, 50, 60, 70 }; 
+  ofstream outfile2("Angulos.txt");
+  
+	return 0;
+}
+
