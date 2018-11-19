299d1e74bc43e6ad3436774ba052f48a4d06139b
Adriana Lucia Caipa Furque <UANDES\al.caipa10@fisicaez12211l.ad.uniandes.edu.co>
2018-11-19 10:18:42 -0500

    PDE futuro

--- PDE.cpp
+++ PDE.cpp
@@ -19,8 +19,36 @@ int main(){
 	double dy = 50.0/10000.0;
 	double dt = dx/v*0.001;
 	cout << dt << endl;
+	double presente[tamanio][tamanio];
+	double futuro[tamanio][tamanio];
 	
-	std::ofstream outfile ("datos1.txt");
+	for(int i=0; i<tamanio;i++){
+	    for(int j=0; j<tamanio; j++){
+		if(((i-50.0)*(i-50.0)+(j-50.0)*(j-50.0))<(5.0*5.0)){
+			presente[i][j]=100;
+			}
+		else {
+		    presente[i][j]=10;
+		    }	
+	cout << presente[i][j] <<" ";
+		}  
+	cout << endl;
+        }
+
+	for(int i=0; i<tamanio;i++){
+		    for(int j=0; j<tamanio; j++){
+			if(((i-50.0)*(i-50.0)+(j-50.0)*(j-50.0))<(5.0*5.0)){
+				futuro[i][j]=100;
+				}
+			else {
+			    futuro[i][j]=presente[i][j]+ (((v*dt)/(dx*dx))*(presente[i+1][j]-2*presente[i][j]+presente[i-1][j])+ ((v*dt)/(dy*dy)*(presente[i][j+1]-2*presente[i][j]+presente[i][j-1])));
+			    }	
+		cout << futuro[i][j] <<" ";
+			}  
+		cout << endl;
+		}
+	
+	
 	
 	return 0;
 }
