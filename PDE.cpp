107ec582f2663be3f9819a09973dcd0cc6f9b869
Adriana Lucia Caipa Furque <UANDES\al.caipa10@fisicaez12211l.ad.uniandes.edu.co>
2018-11-19 09:34:34 -0500

    PDE de nuevo

--- PDE.cpp
+++ PDE.cpp
@@ -9,35 +9,19 @@ int glob;
 #define PI 3.14159
 #define k 1.62
 #define Cp 820
-#define densidad 2.71
-
+#define densidad 2710
+#define diametro 10
 
 int main(){
 	double v= k/(Cp*densidad);
-	int tamanio=40;
-	int iteraciones = 100;
-	double d[tamanio][tamanio];
+	int tamanio=100;
+	double dx = 50.0/10000.0;
+	double dy = 50.0/10000.0;
+	double dt = dx/v*0.001;
+	cout << dt << endl;
+	
 	std::ofstream outfile ("datos1.txt");
 	
-	for(int i=0; i<tamanio; i++)                
-    {   
-      for (int j=0; j<tamanio; j++) d[i][j] = 0;
-    }
-    
-    
-	for(int i=0; i<tamanio; i++) d[i][0] = 100.0;
-	
-	for (int iteraciones = 0; iteraciones < tamanio; iteraciones++){// iteraciones
-		
-		for (int i = 1; i < (tamanio - 1); i++){ //direccion-x
-			
-			for (int j = 1; j < (tamanio - 1); j++){ //direccion-y
-			
-				d[i][j] = 0.25*(d[i+1][j]+d[i-1][j]+d[i][j+1]+d[i][j-1]);
-				
-			}
-		} 
-	}
 	return 0;
 }
 
