92672bbf7af0b1355684e85ee97da0eab18cf39a
AdrianaCaipa <42391099+AdrianaCaipa@users.noreply.github.com>
2018-11-19 13:01:56 -0500

    PDC-fronteras10

--- PDE.cpp
+++ PDE.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cmath>
 #include <math.h>
@@ -13,43 +14,76 @@ int glob;
 #define diametro 10
 
 int main(){
-	double v= k/(Cp*densidad);
-	int tamanio=100;
-	double dx = 50.0/10000.0;
-	double dy = 50.0/10000.0;
-	double dt = dx/v*0.001;
-	cout << dt << endl;
-	double presente[tamanio][tamanio];
-	double futuro[tamanio][tamanio];
-	
-	for(int i=0; i<tamanio;i++){
-	    for(int j=0; j<tamanio; j++){
-		if(((i-50.0)*(i-50.0)+(j-50.0)*(j-50.0))<(5.0*5.0)){
-			presente[i][j]=100;
-			}
-		else {
-		    presente[i][j]=10;
-		    }	
-	cout << presente[i][j] <<" ";
-		}  
-	cout << endl;
-        }
+double v= k/(Cp*densidad);
+int tamanio=50;
+double dx = 0.001;
+double dy = 0.001;
+double dt = dx/v*0.0001;
+double presente[tamanio][tamanio];
+double futuro[tamanio][tamanio];
+double meanpresente=0;
+double meanfuturo=0;
+int total=0;
+ 
+for(int i=0; i<tamanio;i++){
+    for(int j=0; j<tamanio; j++){
+        if(((i-25.0)*(i-25.0)+(j-25.0)*(j-25.0))<(5.0*5.0)){
+            presente[i][j]=100;
+         }
+        else {
+            presente[i][j]=10;
+            total +=1;
+            meanpresente += presente[i][j];
+             }
+}  
+    }
+meanpresente = meanpresente/total;
+cout << meanpresente << endl;
+cout << total << endl;
+for(;;){
+meanfuturo = 0;
+for(int i=0; i<tamanio;i++){
+    for(int j=0; j<tamanio; j++){
+if(((i-25.0)*(i-25.0)+(j-25.0)*(j-25.0))<(5.0*5.0)){
+futuro[i][j]=100;
+}
+else {
+    futuro[i][j]=presente[i][j]+ (((v*dt)/(dx*dx))*(presente[i+1][j]-2*presente[i][j]+presente[i-1][j])+ ((v*dt)/(dy*dy)*(presente[i][j+1]-2*presente[i][j]+presente[i][j-1])));
+    
+    futuro[0][j]=10; //Condiciones de frontera
+futuro[49][j]=10;
+futuro[i][0]=10;
+futuro[i][49]=10;
+ 
+meanfuturo += futuro[i][j]/total;
+//   cout << meanfuturo<<endl;
+}
+//cout << futuro[i][j] << " ";
+}  
+//cout << endl;
+}
+cout << meanfuturo<< " " << meanpresente<< endl;
+if(abs(meanfuturo-meanpresente)<0.01){
+break;
+}
+meanpresente = 0;
+for(int i=0; i<tamanio;i++){
+    for(int j=0; j<tamanio; j++){
+if(((i-25.0)*(i-25.0)+(j-25.0)*(j-25.0))<(5.0*5.0)){
+presente[i][j]=100;
+}
+else {
+    presente[i][j]=futuro[i][j];
+meanpresente += presente[i][j]/total;     
+    }
+}
+// meanpresente = meanfuturo;   
+ 
+    }
+ 
+}
+ 
 
-	for(int i=0; i<tamanio;i++){
-		    for(int j=0; j<tamanio; j++){
-			if(((i-50.0)*(i-50.0)+(j-50.0)*(j-50.0))<(5.0*5.0)){
-				futuro[i][j]=100;
-				}
-			else {
-			    futuro[i][j]=presente[i][j]+ (((v*dt)/(dx*dx))*(presente[i+1][j]-2*presente[i][j]+presente[i-1][j])+ ((v*dt)/(dy*dy)*(presente[i][j+1]-2*presente[i][j]+presente[i][j-1])));
-			    }	
-		cout << futuro[i][j] <<" ";
-			}  
-		cout << endl;
-		}
-	
-	
-	
-	return 0;
+return 0;
 }
 
